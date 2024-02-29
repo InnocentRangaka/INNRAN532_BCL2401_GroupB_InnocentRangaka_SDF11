a8f2038b51a6a5f345f7ed7512774d76b0400580
Innocent Rangaka <itrangaka@gmail.com>
2024-02-29 13:02:18 +0200

    completing index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,20 +4,3 @@ const mobileMenu = document.getElementById("mobile-menu");
 menuBtn.addEventListener("click", function () {
     mobileMenu.classList.toggle("hidden");
 });
-
-// const featuresSection = document.getElementById("features");
-// const features = featuresSection.querySelectorAll(".feature");
-
-// features.forEach(feature => {
-//     feature.addEventListener("mouseover", function(){
-//         const svgBox = this.querySelector("svg").parentElement;
-//         svgBox.classList.add("bg-pink-500");
-//         svgBox.classList.remove("bg-green-500");
-//     });
-
-//     feature.addEventListener("mouseleave",function(){
-//         const svgBox = this.querySelector("svg").parentElement;
-//         svgBox.classList.add("bg-green-500");
-//         svgBox.classList.remove("bg-pink-500");
-//     });
-// });
